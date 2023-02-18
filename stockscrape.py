b8a9376d298bb0099aa6dc9891d989175f1f0f9d
Shreesh-Kulkarni <kshreesh5@gmail.com>
2023-02-19 00:59:40 +0530

    Debugged a index error caused by the xpath

--- stockscrape.py
+++ stockscrape.py
@@ -6,11 +6,21 @@ driver=webdriver.Chrome()
 driver.get(url)
 nameclass='cmc-link'
 name=driver.find_elements(By.CLASS_NAME,nameclass)
-num=[j for j in range(1,12)]
-j=0
-for i in name:
-    namexpath=f'//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[4]/table/tbody/tr[{num[j]}]/td[3]/div/a/div/div/p'
-    cname=i.find_element(By.XPATH,namexpath).text
-    print(cname)
-    j+=1
+num=[j for j in range(1,11)]
+k=0
+d={}
+try:
+    for i in name:
+        value=[]
+        namexpath=f'//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[4]/table/tbody/tr[{num[k]}]/td[3]/div/a/div/div/p'
+        pricexpath=f'//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[4]/table/tbody/tr[{num[k]}]/td[4]/div/a'
+        cname=i.find_element(By.XPATH,namexpath).text
+        cprice=i.find_element(By.XPATH,pricexpath).text
+        value.append(cprice)
+        d[cname]=value
+        k+=1
+except IndexError:
+    print(d)
+
+
 
